7272a4694ec4b15e00ea392c317533ed0745ac26
alex-amayo <alexjamayo@gmail.com>
2024-07-15 23:05:42 -0700

    Minor update on comments

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,12 +1,11 @@
-// https://docs.expo.dev/guides/using-eslint/
 module.exports = {
   extends: ['expo', 'prettier'],
   plugins: ['prettier'],
   ignorePatterns: [
-      '**/node_modules/**', // Ignore all files within node_modules
-      '**/__tests__/**',    // Ignore all files within __tests__ directories
-      'build/',             // Ignore all files within the build directory
-      '*.test.js',          // Ignore all .test.js files
+      '**/node_modules/**', 
+      '**/__tests__/**',    
+      'build/',            
+      '*.test.js',          
       '.expo/**',
       'dist/**',
     ],

a7c3de545cbaeec8ed1b636e2b397aa9b5dde905
alex-amayo <alexjamayo@gmail.com>
2024-07-28 11:01:33 -0700

    Added expo and node_modules to ESLint ignnore

---
+++ eslint.config.js
@@ -0,0 +1,14 @@
+import jsdoc from "eslint-plugin-jsdoc";
+
+export default [
+    {
+        files: ["**/*.js"],
+        plugins: {
+            jsdoc: jsdoc
+        },
+        rules: {
+            "jsdoc/require-description": "error",
+            "jsdoc/check-values": "error"
+        }
+    }
+];

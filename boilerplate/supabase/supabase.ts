dc5b6180d44d7552a4dbd1405707c3d5447ec065
alex-amayo <alexjamayo@gmail.com>
2024-08-13 20:51:04 -0700

    Changed folder structure

--- boilerplate/supabase/supabase.ts
+++ boilerplate/supabase/supabase.ts
@@ -1,6 +1,9 @@
 import 'react-native-url-polyfill/auto';
 import AsyncStorage from '@react-native-async-storage/async-storage';
 import { createClient } from '@supabase/supabase-js';
+import dotenv from 'dotenv';
+
+dotenv.config();
 
 const supabaseUrl = process.env.SUPABASE_URL;
 const supabaseAnonKey = process.env.SUPABASE_ANON_KEY;

3ac57f839cc2762b8fd1c5ef72e9eee8784e5d02
alex-amayo <alexjamayo@gmail.com>
2024-08-13 23:05:52 -0700

    Changed to use Expo environment variables

--- boilerplate/supabase/supabase.ts
+++ boilerplate/supabase/supabase.ts
@@ -1,12 +1,9 @@
 import 'react-native-url-polyfill/auto';
 import AsyncStorage from '@react-native-async-storage/async-storage';
 import { createClient } from '@supabase/supabase-js';
-import dotenv from 'dotenv';
 
-dotenv.config();
-
-const supabaseUrl = process.env.SUPABASE_URL;
-const supabaseAnonKey = process.env.SUPABASE_ANON_KEY;
+const supabaseUrl = process.env.EXPO_PUBLIC_SUPABASE_URL;
+const supabaseAnonKey = process.env.EXPO_PUBLIC_SUPABASE_ANON_KEY;
 
 if (!supabaseUrl || !supabaseAnonKey) {
   throw new Error('Missing Supabase URL or Anon Key in environment variables');
